23d348f81d284dad2f0f2abb6ddcf7a6c5f11da4
David-Sulub1 <69609893+David-Sulub1@users.noreply.github.com>
2022-11-16 20:44:58 -0600

    Update streamlit_ariana.py

--- streamlit_ariana.py
+++ streamlit_ariana.py
@@ -2,7 +2,8 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-dfPoliza = pd.read_excel("A:\\UADY\\7TOSEMESTRE\\Herramientas III\\Proyecto\\ArianaConsultaPagos.xlsx")
+#dfPoliza = pd.read_excel("A:\\UADY\\7TOSEMESTRE\\Herramientas III\\Proyecto\\ArianaConsultaPagos.xlsx")
+dfPoliza = pd.read_excel("https://github.com/David-Sulub1/streamlitparcial1/blob/main/ArianaConsultaPagos.xlsx?raw=true")
 dfPoliza.head()
 dfPoliza.columns
 
